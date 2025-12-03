7d8939afa65232ba54ffd09f9a5821aba131b8a1
Simon Thornington <simon.thornington@gmail.com>
2025-12-03 00:10:58 -0500

    wip

--- run_container.sh
+++ run_container.sh
@@ -1,1 +1,3 @@
-docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864  --name fastai -p 8888:8888 -v ~/projects/fastai:/fastai fastai:latest
+docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864  --name fastai -p 8888:8888 \
+    -v $HOME/.cache:/home/sthornington/.cache \
+    -v ~/projects/fastai:/fastai fastai:latest
